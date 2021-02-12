301678157fa6ea22f5d18e5e24c4f71c6ced50f0
anonymous19583 <75793757+anonymous19583@users.noreply.github.com>
2021-02-12 13:09:56 +0900

    Add files via upload

---
+++ networks/__init__.py
@@ -0,0 +1,8 @@
+import sys
+path_root = '/path_root'
+sys.path.append(path_root)
+
+from adv_training.networks.simpleConv import *
+from adv_training.networks.simpleMLP import *
+from adv_training.networks.resnet import *
+from adv_training.networks.wide_resnet import *
